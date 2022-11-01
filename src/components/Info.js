3e5497bf9eefa23cfb9c50849829d58f6b4d3543
torback <tlorback@gmail.com>
2022-10-31 20:46:41 -0600

    Update ui

--- src/components/Info.js
+++ src/components/Info.js
@@ -19,6 +19,32 @@ const Info = ({ account, accountBalance, maxTokens }) => {
           <strong>Your percentage of the total supply: </strong>0%
         </h4>
       )}
+      <h4>
+        <strong>
+          RUSH Token contract verified
+          <a
+            href="https://goerli.etherscan.io/address/0x8188b6859b2ddd72Df01dCe8649ae980C0092f25"
+            target="_blank"
+            rel="noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            &nbsp;here
+          </a>
+        </strong>
+      </h4>
+      <h4>
+        <strong>
+          Crowdsale contract verified
+          <a
+            href="https://goerli.etherscan.io/address/0xe1Fbcb1525A941f4f6409171f3Fe59BeABE1b4D1"
+            target="_blank"
+            rel="noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            &nbsp;here
+          </a>
+        </strong>
+      </h4>
     </div>
   )
 }
